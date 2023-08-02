05772bf42da0541761917ac7d248ff2401acb151
Aditya Kumar <aditya.ak4747@gmail.com>
2023-08-02 21:45:39 +0530

    Ui Commit 2

--- app/src/main/java/com/example/tripit/HomeActivity.kt
+++ app/src/main/java/com/example/tripit/HomeActivity.kt
@@ -1,11 +1,48 @@
 package com.example.tripit
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tripit.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding= ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpTabBar()
+
+//        setContentView(R.layout.activity_home)
+    }
+    private fun setUpTabBar() {
+        binding.chipNavigationBar.setOnItemSelectedListener{
+            when(it){
+                R.id.menu_home -> {
+//                    binding.textMain.text="Home"
+//                    binding.textMain.setTextColor(Color.BLUE)
+
+                }
+                R.id.menu_message ->{
+//                    binding.textMain.text="Message"
+//                    binding.textMain.setTextColor(Color.RED)
+
+                }
+                R.id.menu_profile -> {
+//                    binding.textMain.text="Profile"
+//                    binding.chipNavigationBar.showBadge(R.id.menu_settings)
+//                    binding.textMain.setTextColor(Color.MAGENTA)
+
+                }
+                R.id.menu_settings -> {
+//                    binding.textMain.text="Settings"
+//                    binding.chipNavigationBar.dismissBadge(R.id.menu_settings)
+//                    binding.textMain.setTextColor(Color.GREEN)
+
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/tripit/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/tripit/fragments/HomeFragment.kt
@@ -1,10 +1,11 @@
-package com.example.tripit
+package com.example.tripit.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.tripit.R
 
 class HomeFragment : Fragment() {
 
