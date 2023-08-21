881fde63b1cee392ccb10b3f7c2378a49122db52
Aditya Kumar <aditya.ak4747@gmail.com>
2023-08-21 22:13:18 +0530

    Ui Commit 4

--- app/src/main/java/com/example/tripit/HomeActivity.kt
+++ app/src/main/java/com/example/tripit/HomeActivity.kt
@@ -3,7 +3,12 @@ package com.example.tripit
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.tripit.databinding.ActivityHomeBinding
+import com.example.tripit.fragments.HomeFragment
+import com.example.tripit.fragments.ItineraryFragment
+import com.example.tripit.fragments.PostFragment
+import com.example.tripit.fragments.SettingsFragment
 
 class HomeActivity : AppCompatActivity() {
 
@@ -13,7 +18,11 @@ class HomeActivity : AppCompatActivity() {
 
         binding= ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         setUpTabBar()
+        binding.chipNavigationBar.setItemSelected(R.id.menu_home, true)
+
+        loadFragment(HomeFragment())
 
 //        setContentView(R.layout.activity_home)
     }
@@ -21,22 +30,29 @@ class HomeActivity : AppCompatActivity() {
         binding.chipNavigationBar.setOnItemSelectedListener{
             when(it){
                 R.id.menu_home -> {
+                    loadFragment(HomeFragment())
+
 //                    binding.textMain.text="Home"
 //                    binding.textMain.setTextColor(Color.BLUE)
 
                 }
-                R.id.menu_message ->{
+                R.id.menu_nav ->{
+                    loadFragment(ItineraryFragment())
+
 //                    binding.textMain.text="Message"
 //                    binding.textMain.setTextColor(Color.RED)
 
                 }
                 R.id.menu_profile -> {
+                    loadFragment(SettingsFragment())
+
 //                    binding.textMain.text="Profile"
 //                    binding.chipNavigationBar.showBadge(R.id.menu_settings)
 //                    binding.textMain.setTextColor(Color.MAGENTA)
 
                 }
-                R.id.menu_settings -> {
+                R.id.menu_post -> {
+                    loadFragment(PostFragment())
 //                    binding.textMain.text="Settings"
 //                    binding.chipNavigationBar.dismissBadge(R.id.menu_settings)
 //                    binding.textMain.setTextColor(Color.GREEN)
@@ -44,5 +60,11 @@ class HomeActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
     }
 }
