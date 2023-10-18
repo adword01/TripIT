daa779b6e0c0cd7fb097eb72dd4780741fe304b6
Aditya Kumar <aditya.ak4747@gmail.com>
2023-10-18 11:41:32 +0530

    Ui Commit 9 with post functionality ui

--- app/src/main/java/com/example/tripit/fragments/CreatePostFragment.kt
+++ app/src/main/java/com/example/tripit/fragments/CreatePostFragment.kt
@@ -1,27 +1,145 @@
 package com.example.tripit.fragments
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.ExifInterface
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.example.tripit.R
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.squareup.picasso.Picasso
+import java.io.IOException
 
 
 class CreatePostFragment : Fragment() {
+    private lateinit var imageView: ImageView
+    private lateinit var databaseReference: DatabaseReference
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
+    companion object {
+        const val REQUEST_IMAGE_PICK = 100
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_post, container, false)
+        val view: View =
+            inflater.inflate(com.example.tripit.R.layout.fragment_create_post, container, false)
+
+        // Initialize Firebase database reference
+        databaseReference = FirebaseDatabase.getInstance().reference.child("posts")
+
+        imageView = view.findViewById<ImageView>(com.example.tripit.R.id.imageView11)
+
+        imageView.setOnClickListener {
+            pickImageFromGallery()
+        }
+
+        return view
+    }
+
+    private fun pickImageFromGallery() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                REQUEST_IMAGE_PICK
+            )
+        } else {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, REQUEST_IMAGE_PICK)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_IMAGE_PICK) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                pickImageFromGallery()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_PICK && resultCode == Activity.RESULT_OK && data != null) {
+            val imageUri: Uri = data.data!!
+            val imagePath = getRealPathFromURI(imageUri)
+
+            Picasso.get().load(imageUri).into(imageView)
+
+            try {
+                // Extract location information from image metadata
+                val exif = ExifInterface(imagePath)
+                val latLong = FloatArray(2)
+                if (exif.getLatLong(latLong)) {
+                    val latitude = latLong[0].toDouble()
+                    val longitude = latLong[1].toDouble()
+
+                    // Save the location to Firebase
+                    saveLocationToFirebase(imageUri.toString(), latitude, longitude)
+                } else {
+
+                    val location_lyt= view?.findViewById<LinearLayout>(R.id.location_lyt)
+                    location_lyt?.visibility=View.VISIBLE
+
+                    
+//                    Toast.makeText(requireContext(),"No coordinates",Toast.LENGTH_SHORT).show()
+
+                    // Handle the case where the image does not contain GPS coordinates.
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun getRealPathFromURI(uri: Uri): String {
+        val projection = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = activity?.contentResolver?.query(uri, projection, null, null, null)
+        val columnIndex = cursor?.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        cursor?.moveToFirst()
+        val path = cursor?.getString(columnIndex ?: 0)
+        cursor?.close()
+        return path ?: ""
+    }
+
+    private fun saveLocationToFirebase(imageUrl: String, latitude: Double, longitude: Double) {
+        // Create a HashMap to store location information
+        val locationData = HashMap<String, Any>()
+        locationData["latitude"] = latitude
+        locationData["longitude"] = longitude
+        locationData["imageUrl"] = imageUrl
+
+        // Push the data to Firebase
+        val postId = databaseReference.push().key
+        if (postId != null) {
+            databaseReference.child(postId).setValue(locationData)
+            Toast.makeText(requireContext(),"Done",Toast.LENGTH_SHORT).show()
+        }
+        Toast.makeText(requireContext(),"Empty",Toast.LENGTH_SHORT).show()
+
     }
 }
--- app/src/main/java/com/example/tripit/fragments/PostFragment.kt
+++ app/src/main/java/com/example/tripit/fragments/PostFragment.kt
@@ -5,25 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.tripit.R
+import com.example.tripit.databinding.FragmentPostBinding
 
 
 class PostFragment : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
+    private lateinit var binding: FragmentPostBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post, container, false)
+
+        binding=  FragmentPostBinding.inflate(inflater,container,false)
+
+        binding.addNew.setOnClickListener {
+
+            val createPostFragment = CreatePostFragment()
+
+            // Get the FragmentManager and start a FragmentTransaction
+            val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
+            val transaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+            // Replace the current fragment with the "Create Post" fragment
+            transaction.replace(R.id.container, createPostFragment)
+            // You can add the transaction to the back stack if you want to navigate back
+            // transaction.addToBackStack(null)
+
+            // Commit the transaction
+            transaction.commit()
+        }
+
+        return binding.root
     }
+
 
 }
